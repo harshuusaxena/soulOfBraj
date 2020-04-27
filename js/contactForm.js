6a26896137b14ed761adc5395ad21d809aa23e17
harshuusaxena <importantemail545@gmail.com>
2020-04-27 12:02:33 +0530

    contact form update in index page only

---
+++ js/contactForm.js
@@ -0,0 +1,17 @@
+$("#submitButton").click(function(e){
+    
+    var firstName = $("#firstName").val();
+    var lastName = $("#lastName").val();
+    var phone = $("#phone").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+    var x = $("#email").val();
+    var atposition = x.indexOf("@");
+    var dotposition = x.lastIndexOf(".");
+    
+    
+    if(firstName != ""){
+        $("#submitButton").rem
+    }
+    
+});
---
+++ js/firebase_init.js
@@ -0,0 +1,13 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyAMHf9JASbmtQd2iJmbdE9Aq84_xfxN-3A",
+    authDomain: "soulofbraj-80ea9.firebaseapp.com",
+    databaseURL: "https://soulofbraj-80ea9.firebaseio.com",
+    projectId: "soulofbraj-80ea9",
+    storageBucket: "soulofbraj-80ea9.appspot.com",
+    messagingSenderId: "425992961607",
+    appId: "1:425992961607:web:5e730e6f2756faec93bb62",
+    measurementId: "G-654THJR16J"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
